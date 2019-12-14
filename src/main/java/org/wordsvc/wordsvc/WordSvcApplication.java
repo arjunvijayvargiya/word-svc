efac4b3274c4e42dc2aa22ad923972529aee5a71
arjunvijayvargiya <arjun.vijay20@gmail.com>
2019-12-14 19:28:51 +0530

    Initial commit

---
+++ src/main/java/org/wordsvc/wordsvc/WordSvcApplication.java
@@ -0,0 +1,13 @@
+package org.wordsvc.wordsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wordsvc/wordsvc/WordSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wordsvc.wordsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
